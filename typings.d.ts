7a060c73604a6dfb820615dee743e70bdb6c2002
RoniJackVituli <Vituli@exapander.com>
2023-05-28 13:01:29 +0300

    new struct

--- typings.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.scss";
-declare module "*.jpg";
-declare module "*.JPG";
-declare module "*.jpeg";
-declare module "*.png";
-declare module "*.svg";
-declare module "*.css";
-declare module "*.webp";
-

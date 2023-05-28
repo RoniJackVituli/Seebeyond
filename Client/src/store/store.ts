fa8b1fc5ddcd43b8d6f109ca225cd637ad3b05ea
Boaz Bitton <boaz2119@gmail.com>
2023-05-28 23:40:37 +0300

    Added localstorage with expration time

--- Client/src/store/store.ts
+++ Client/src/store/store.ts
@@ -1,12 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./User/User-slice";
-
+import  checkTime  from '../utils/function/checkTime'
 
 const store = configureStore({
-    reducer:{
-        'user': userSlice.reducer
-    }
-})
+  reducer: {
+    user: userSlice.reducer,
+  },
+  preloadedState: {
+    user: {
+      user: localStorage.getItem("user")&&checkTime()
+        ? JSON.parse(localStorage.getItem("user")!).user
+        : null,
+    },
+  },
+  
+  
+});
+const expiresIn: number =  5 * 60 * 1000;
+
+store.subscribe(() => {
+  const { user } = store.getState();
+  const expirationTime: number = new Date().getTime() + expiresIn;
+  localStorage.setItem("user", JSON.stringify(user));
+  localStorage.setItem("time", expirationTime.toString());
+});
+
 
 
 export type RootState = ReturnType<typeof store.getState>;

e3ba2b0b99b89a21c222f12d0c317f6d9cc7308b
RoniJackVituli <Vituli@exapander.com>
2023-05-28 13:45:21 +0300

    add a userdetail type

---
+++ Client/src/utils/interfaces/userDetails.ts
@@ -0,0 +1,9 @@
+export default interface UserDetails {
+    type: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+    phone: string,
+    password: string,
+    confirm_password: string,
+}
